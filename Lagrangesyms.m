61fa118cbaee3e83c57f0c4a1a26f93c5b6e8533
Npdminh <dangminh1203@gmail.com>
2025-12-26 09:24:48 +0700

    Add files via upload

--- Lagrangesyms.m
+++ Lagrangesyms.m
@@ -1,18 +1,18 @@
 function result = Lagrangesyms(xa, ya)
-    syms x;                 % dùng symbolic
+    syms x;                 
     n = length(ya);         
-    sum_poly = 0;           % không dùng tên sum
+    sum_poly = 0;           
 
     for i = 1:n
-        product = ya(i);    % nhân với y_i
+        product = ya(i);    
         for j = 1:n
             if i ~= j
-                % SỬA NGOẶC cho đúng công thức
+                
                 product = product * (x - xa(j)) / (xa(i) - xa(j));
             end
         end
         sum_poly = sum_poly + product;
     end
 
-    result = expand(sum_poly);   % rút gọn đa thức
+    result = expand(sum_poly);   
 end
